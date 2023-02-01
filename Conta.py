7b1a9afc0402bb3639123a20b9509fcd139c9071
Fawke-s <83735637+Marlon-Santiago@users.noreply.github.com>
2023-01-31 22:28:33 -0300

    criando cliente

--- Conta.py
+++ Conta.py
@@ -3,17 +3,32 @@ class Conta:
     def __init__(self, numero, titular, saldo, limite):
         
         print(f'Construindo objeto... {self}')
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     
     def extrato(self):
-        print(f'Saldo de {self.saldo} do titular {self.titular}')
+        print(f'Saldo de {self.__saldo} do titular {self.__titular}')
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
     
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @limite.settersetter
+    def limite(self, limite):
+        self.__limite = limite
+
